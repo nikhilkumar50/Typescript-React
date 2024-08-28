b7b8917485a287bf98d7f5a660bb253d96596b8a
nikhilkumar50 <nikhilkumarray9889@gmail.com>
2024-08-28 12:58:22 +0530

    useState in Typescript

--- React-Typescript/src/App.tsx
+++ React-Typescript/src/App.tsx
@@ -1,10 +1,50 @@
-import Box from "./components/Box";
+import { FormEvent, useState } from "react";
+
+interface Person {
+  name: string;
+  age: number;
+}
 
 const App = () => {
+  const [user, setUser] = useState<Person>();
+
+  const submitHandler = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+
+    console.log(user);
+    if (user) {
+      setUser({ ...user, name: "", age: 0 });
+    }
+  };
+
   return (
     <div>
-      <Box label="Enter Your Name" value={"Nikhil Kumar"} onChange={()=>{}}/>
-      
+      <form onSubmit={submitHandler}>
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          placeholder="Enter Your Name"
+          name="name"
+          value={user?.name || ""}
+          onChange={(e) =>
+            setUser((prev) => ({ ...prev!, name: e.target.value }))
+          }
+          autoComplete="off"
+        />
+
+        <label htmlFor="age">Age</label>
+        <input
+          type="number"
+          placeholder="Enter Your Age"
+          name="age"
+          value={user?.age || ""}
+          onChange={(e) =>
+            setUser((prev) => ({ ...prev!, age: Number(e.target.value) }))
+          }
+        />
+
+        <button type="submit">Register</button>
+      </form>
     </div>
   );
 };
